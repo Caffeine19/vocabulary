7dbf222c99e1c7926a73345b7f0ff7f5560778e7
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2024-01-15 00:47:16 +0800

    :construction: working on SnippetItemGroup

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import StatusTabGroup from './components/StatusTab'
 import Divider from './components/Divider.vue'
 import FolderTabGroup from './components/FolderTabGroup.vue'
 import TagTabGroup from './components/TagTabGroup.vue'
+import Search16 from './components/Icon/Search16.vue'
+import Pencil16 from './components/Icon/Pencil16.vue'
 </script>
 <template>
   <div class="flex flex-col w-screen h-screen overflow-hidden">
@@ -23,7 +25,39 @@ import TagTabGroup from './components/TagTabGroup.vue'
         </div>
       </nav>
       <Divider type="vertical"></Divider>
-      <div></div>
+
+      <div class="p-6 space-y-4">
+        <div class="flex items-center space-x-4">
+          <div class="relative min-w-[20rem]">
+            <Search16
+              class="absolute top-1/2 -translate-y-1/2 left-2.5 dark:fill-primer-dark-gray-400"
+            ></Search16>
+            <input
+              placeholder="Type to search"
+              class="pl-10 pr-2.5 py-1.5 bg-transparent dark:placeholder-primer-dark-gray-400 border outline-none dark:border-primer-dark-gray-600 rounded-md text-sm font-normal dark:text-primer-dark-gray-100 w-full"
+            />
+          </div>
+          <button
+            class="flex items-center space-x-1.5 px-2.5 py-1 rounded-md dark:bg-primer-dark-green-500 dark:hover:bg-primer-dark-green-400 transition-colors"
+          >
+            <span class="dark:text-primer-dark-white text-base font-normal">New Snippet</span>
+            <Pencil16 class="dark:fill-primer-dark-white"></Pencil16>
+          </button>
+        </div>
+
+        <div class="rounded-md border dark:border-primer-dark-gray-600">
+          <div class="dark:bg-primer-dark-gray-800 flex justify-between px-3 py-1.5">
+            <div></div>
+            <div>
+              <div class="flex items-center">
+                <span>Sort</span>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <Divider type="vertical"></Divider>
       <div></div>
     </main>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,16 @@ export default {
           400: '#6E7681',
           600: '#30363D',
           700: '#21262D',
+          800: '#161B22',
           900: '#0D1117'
         },
         'primer-dark-white': '#FFFFFF',
         'primer-dark-blue': {
           500: '#1F6FEB'
+        },
+        'primer-dark-green': {
+          400: '#2EA043',
+          500: '#238636'
         }
       }
     }
