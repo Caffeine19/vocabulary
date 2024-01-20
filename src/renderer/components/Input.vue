009bfb801a70e9c186cfb87aa0ff9bbf63360d6a
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2024-01-21 04:33:52 +0800

    :lipstick: adjust input style on focus

--- src/renderer/components/Input.vue
+++ src/renderer/components/Input.vue
@@ -9,7 +9,7 @@ defineEmits<{ 'update:value': [e: string] }>()
     <input
       @input="$emit('update:value', ($event.target as HTMLInputElement).value)"
       :placeholder="placeholder"
-      class="pl-10 pr-2.5 py-1.5 bg-transparent dark:placeholder-primer-dark-gray-400 border outline-none dark:border-primer-dark-gray-600 rounded-md text-sm font-normal dark:text-primer-dark-gray-100 w-full fira-code"
+      class="pl-10 pr-2.5 py-1.5 bg-transparent dark:placeholder-primer-dark-gray-400 border outline-none dark:border-primer-dark-gray-600 rounded-md text-sm font-normal dark:text-primer-dark-gray-100 w-full fira-code dark:bg-primer-dark-gray-900 dark:focus:border-primer-dark-blue-400 transition-colors"
     />
   </div>
 </template>
