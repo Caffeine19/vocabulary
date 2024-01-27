0a7ab7e6e0189a85a93fdaf777068a8588e7c17f
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2024-01-27 20:13:48 +0800

    :lipstick: update TagTab style

--- src/renderer/components/TagTab.vue
+++ src/renderer/components/TagTab.vue
@@ -14,29 +14,20 @@ defineProps({
 </script>
 <template>
   <li
-    class="group flex items-center space-x-1 w-full"
-    :class="selected ? 'cursor-default' : 'cursor-pointer'"
+    class="group flex items-center w-full justify-between px-2 py-1.5 rounded-md grow transition-colors space-x-4 after:w-1 after:h-6 after:rounded-md after:dark:dark:bg-primer-dark-blue-500 after:-left-2 after:absolute after:transition-opacity relative"
+    :class="
+      selected
+        ? 'cursor-default dark:bg-primer-dark-gray-200/[0.08] dark:hover:bg-primer-dark-gray-200/[0.12] after:opacity-100'
+        : 'cursor-pointer dark:hover:bg-primer-dark-gray-200/[0.12] after:opacity-0'
+    "
   >
-    <div
-      class="w-1 h-6 rounded-md dark:bg-primer-dark-blue-500"
-      :class="selected ? 'opacity-100' : 'opacity-0'"
-    ></div>
-    <div
-      class="flex items-center justify-between px-2 py-1.5 rounded-md grow transition-colors space-x-4"
-      :class="
-        selected
-          ? ' dark:bg-primer-dark-gray-200/[0.08] dark:group-hover:bg-primer-dark-gray-200/[0.12] '
-          : ' dark:group-hover:bg-primer-dark-gray-200/[0.12] '
-      "
-    >
-      <div class="flex items-center space-x-2">
-        <Tag16 class="dark:fill-primer-dark-gray-400"></Tag16>
-        <span class="dark:text-primer-dark-gray-100 text-sm font-normal fira-code">{{
-          tag.name
-        }}</span>
-      </div>
-
-      <Badge :num="tag.snippetCount"></Badge>
+    <div class="flex items-center space-x-2">
+      <Tag16 class="dark:fill-primer-dark-gray-400"></Tag16>
+      <span class="dark:text-primer-dark-gray-100 text-sm font-normal fira-code">{{
+        tag.name
+      }}</span>
     </div>
+
+    <Badge :num="tag.snippetCount"></Badge>
   </li>
 </template>
