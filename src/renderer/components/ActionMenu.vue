1bdbd898522408757b736fb419b1320d80f38cfe
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2024-02-06 14:12:00 +0800

    :dizzy: add transition for ActionMenu

--- src/renderer/components/ActionMenu.vue
+++ src/renderer/components/ActionMenu.vue
@@ -19,27 +19,41 @@ onClickOutside(menuRef, () => {
 </script>
 <template>
   <Teleport to="body">
-    <ul
-      class="rounded-xl dark:bg-primer-dark-gray-800 top-1/2 left-1/2 min-w-48 fixed z-10"
-      style="box-shadow: 0px 8px 24px 0px #010409"
-      :style="{ top: actionMenuPosition.top + 'px', left: actionMenuPosition.left + 'px' }"
-      v-if="isActionMenuOpen"
-      ref="menuRef"
-    >
-      <template v-for="(section, index) in actionMenuOptions" :key="index">
-        <ul class="p-2">
-          <ActionMenuItem
-            v-for="(item, index) in section"
-            :key="index"
-            v-bind="item"
-            @action-execute="closeActionMenu"
-          ></ActionMenuItem>
-        </ul>
-        <Divider
-          class="dark:!bg-primer-dark-gray-600/[0.48]"
-          v-if="index !== actionMenuOptions.length - 1"
-        ></Divider>
-      </template>
-    </ul>
+    <Transition>
+      <ul
+        class="rounded-xl dark:bg-primer-dark-gray-800 top-1/2 left-1/2 min-w-48 fixed z-10"
+        style="box-shadow: 0px 8px 24px 0px #010409"
+        :style="{ top: actionMenuPosition.top + 'px', left: actionMenuPosition.left + 'px' }"
+        v-if="isActionMenuOpen"
+        ref="menuRef"
+      >
+        <template v-for="(section, index) in actionMenuOptions" :key="index">
+          <ul class="p-2">
+            <ActionMenuItem
+              v-for="(item, index) in section"
+              :key="index"
+              v-bind="item"
+              @action-execute="closeActionMenu"
+            ></ActionMenuItem>
+          </ul>
+          <Divider
+            class="dark:!bg-primer-dark-gray-600/[0.48]"
+            v-if="index !== actionMenuOptions.length - 1"
+          ></Divider>
+        </template>
+      </ul>
+    </Transition>
   </Teleport>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
