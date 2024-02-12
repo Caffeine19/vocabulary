6fa8cc75296071ec973eae12cbae4ca45e001e1c
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2024-02-12 23:56:58 +0800

    :sparkles: finish Tooltip

--- src/renderer/components/Tooltip.vue
+++ src/renderer/components/Tooltip.vue
@@ -10,14 +10,20 @@ const triggerRef = ref<null | HTMLElement>(null)
 const isShow = ref(false)
 const tooltipPosition = reactive({ top: 0, left: 0 })
 const groupRef = ref<null | HTMLElement>(null)
+const tooltipRef = ref<null | HTMLElement>(null)
+
+const gap = 6
 
 watch(isShow, (newVal) => {
   if (newVal) {
     nextTick(() => {
-      if (triggerRef.value) {
+      if (triggerRef.value && tooltipRef.value) {
         const triggerRect = triggerRef.value.getBoundingClientRect()
 
-        tooltipPosition.left = triggerRect.left
+        //vertical align center
+        tooltipPosition.left =
+          triggerRect.left - (tooltipRef.value.clientWidth - triggerRect.width) / 2
+        tooltipPosition.top = triggerRect.bottom + gap
       }
     })
   }
@@ -28,12 +34,27 @@ watch(isShow, (newVal) => {
     <div class="" ref="triggerRef">
       <slot name="trigger"></slot>
     </div>
-    <div
-      class="dark:bg-primer-dark-gray-400 dark:text-primer-dark-white fira-code group-hover:flex fixed z-10 items-center justify-center px-2 py-1 text-sm rounded-md"
-      :style="{ top: tooltipPosition.top + 'px', left: tooltipPosition.left + 'px' }"
-      v-show="isShow"
-    >
-      <span>{{ text }}</span>
-    </div>
+
+    <Transition>
+      <div
+        class="dark:bg-primer-dark-gray-400 dark:text-primer-dark-white fira-code group-hover:flex fixed z-10 items-center justify-center px-2 py-1 text-sm rounded-md"
+        :style="{ top: tooltipPosition.top + 'px', left: tooltipPosition.left + 'px' }"
+        v-show="isShow"
+        ref="tooltipRef"
+      >
+        <span>{{ text }}</span>
+      </div>
+    </Transition>
   </div>
 </template>
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
